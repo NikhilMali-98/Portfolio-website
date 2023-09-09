b31d732fbac377fc0f3564fcdbcdab5171638849
Mali Nikhil Pratap <134043327+NikhilMali-98@users.noreply.github.com>
2023-09-09 12:58:51 +0530

    Update index.jsx
    added email facility

--- src/containers/contact/index.jsx
+++ src/containers/contact/index.jsx
@@ -3,8 +3,31 @@ import {BiSolidContact} from 'react-icons/bi';
 import PageHeaderContent from "../../components/pageHeaderContent";
 import './styles.scss'
 import {Animate} from "react-simple-animate"
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+// export const ContactUs = () => {
+  
+
 
 const Contact=()=>{
+
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_qqdy61p', 'template_zpx201o', form.current, 'h0GVy5ELfsGvdLdOG')
+      .then((result) => {
+          console.log(result.text);
+          alert("Send Sucessfully.");
+
+      }, (error) => {
+          console.log(error.text);
+          alert("Enter Valid email id");
+      });
+  };
+
     return(
         // <div>
         //     Contact Page
@@ -50,25 +73,28 @@ const Contact=()=>{
                 } 
                 >
                     <div className="contact__content__form">
+                    <form ref={form} onSubmit={sendEmail}>
                         <div className="contact__content__form__control-wrapper">
                             <div className="name">
-                                <input required name="name" className="iname" type="{'text'}"></input>
+                                <input required name="user_name" className="iname" type="{'text'}"></input>
                                 <label htmlFor="name" className="lname">Name :</label>
                             </div>
                             <div className="email">
-                            <input required name="email" className="iemail" type="{'text'}"></input>
+                            <input required name="user_email" className="iemail" type="{'text'}"></input>
                                 <label htmlFor="email" className="lemail">Email :</label>
                             </div>
                             <div className="description">
-                            <textarea required rows={5} name="description" className="idescription" type="{'text'}"></textarea>
+                            <textarea required rows={5} name="message" className="idescription" type="{'text'}"></textarea>
                                 <label htmlFor="description" className="ldescription">Description :</label>
                             </div>
                         </div>
                         <button>Submit</button>
+                        </form>
                     </div>
                 </Animate>
             </div>
     </section>
     )
-}
+};
 export default Contact;
+
